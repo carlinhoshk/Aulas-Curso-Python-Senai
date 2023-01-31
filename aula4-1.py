76a1606eefaa48f9276000a4b34ade2f95e69b91
carlos Oliveira <40872405+carlinhoshk@users.noreply.github.com>
2023-01-31 16:39:55 -0300

    logica if e boleanos em aula

---
+++ aula4-1.py
@@ -0,0 +1,10 @@
+# idade = int(input("Digite sua idade: "))
+
+# if idade >= 18:
+#     print("Maior de idade ")
+# else:
+#     print("Não é maior de idade ")    
+
+
+maior = 'b' > 'a'
+print(maior)
---
+++ aula4-2.py
@@ -0,0 +1,68 @@
+# 31/01/2023 - 15:58
+
+# Exercicio A
+a = 500 > 350
+print(a)
+
+# Exercicio B
+b = 500 / 2 < 45
+print(b)
+
+# Exercicio C
+c = 2**4 == 16
+print(c)
+
+# Exercicio D
+d = 47 == 200
+print(d)
+
+# Exercicio E
+e = 100 * 2 < 2**8
+print(e)
+
+# Exercicio F
+f = 333 < 600
+print(f)
+
+# Exercicio G
+g = 333 > 600
+print(g)
+
+# Exercicio H
+h = 4 == 2**3
+print(h)
+
+# Exercicio I
+joao = 1.72
+juliana = 1.72
+
+i = joao > juliana
+print(i)
+
+# Exercicio J
+henrique_salario = 500
+jorge_salario = (henrique_salario * 2) / 3
+j = henrique_salario > jorge_salario 
+print(jorge_salario)
+print(j)
+
+# Exercicio K
+
+tio = 1967
+tia = 1972
+
+tio_nasc = 1967
+tia_nasc = 1972
+
+decada_tio = tio_nasc // 10 * 10
+decada_tia = tia_nasc // 10 * 10
+
+if decada_tio == decada_tia:
+    print("Eles nasceram na mesma década")
+else:
+    print("Eles nasceram em décadas diferentes")
+
+
+# Exercicio L
+l = not(45>27)
+print(l)
